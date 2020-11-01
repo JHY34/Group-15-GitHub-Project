be90156dfef603fbdba819263e755b2732b2b055
BanuTorun <banu.torun@hotmail.com>
2020-11-01 15:02:19 -0600

    third commit

--- src/main/java/TC_Banu.java
+++ src/main/java/TC_Banu.java
@@ -1,5 +1,7 @@
 public class TC_Banu {
     public static void main(String[] args) {
+
         System.out.println("Iam working on my TC");
+        System.out.println("Test, test, test");
     }
 }
